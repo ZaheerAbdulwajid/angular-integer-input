69897dec6f39d19a2112f385bd396b563c687c38
Zaheer <zaheer.aws@gmail.com>
2017-03-06 18:33:34 +0500

    integer-input directive

---
+++ angular-integer-input.js
@@ -0,0 +1,27 @@
+//
+integer-input
+// ==================
+//
+// Directive that makes the inputs with `integer` to be
+// available as integer in the model.
+//
+(function () {
+  'use strict';
+
+  angular.module('integer-input', [])
+
+  .directive('integer', [
+    '$parse',
+    function ($parse) {
+      return {
+        require: 'ngModel',
+        link: function(scope, ele, attr, ctrl){
+            ctrl.$parsers.unshift(function(viewValue){
+                return parseInt(viewValue, 10);
+            });
+        }
+      };
+    }
+  ]);
+
+})();
